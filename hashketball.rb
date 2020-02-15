0ac7fa9a7bd2367d32b7f761876d0328ec5cf8b8
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 04:09:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,14 +16,24 @@ def game_hash
                :slam_dunks => 2
               },
                {:player_name => "Bismack Biyombo",
-               :number => 0
-               :shoe => 16
-               :points => 12
-               :rebounds => 4
-               :assists => 7
-               :steals => 22
-               :blocks => 15
+               :number => 0,
+               :shoe => 16,
+               :points => 12,
+               :rebounds => 4,
+               :assists => 7,
+               :steals => 22,
+               :blocks => 15,
                :slam_dunks => 10
+               },
+               {:player_name => "Desagna Diop",
+               :number =>2,
+               :shoe => 14,
+               :points =>24,
+               :rebounds =>12,
+               :assists =>12,
+               :steals =>4,
+               :blocks =>5,
+               :slam_dunks =>5 
               ]
  }
  
