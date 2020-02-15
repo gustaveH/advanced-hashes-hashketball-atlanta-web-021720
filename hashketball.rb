ac414c67a0f688254d73252f3d0dbbfde70cfabb
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 04:39:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,7 +45,7 @@ def game_hash
               :blocks => 1,
               :slam_dunks => 0
                },
-               {:player_name => "Brendan Heywood",
+               {:player_name => "Kemba Walker",
               :number => 33,
               :shoe => 15,
               :points => 6,
