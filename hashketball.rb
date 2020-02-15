f310f9e85711b7c38c59b589abbebfa9314387e9
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 04:12:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -34,6 +34,16 @@ def game_hash
                :steals =>4,
                :blocks =>5,
                :slam_dunks =>5 
+               },
+               {:player_name => "Ben Gordon",
+               :number =>
+               :shoe =>
+               :points =>
+               :rebounds =>
+               :assists =>
+               :steals =>
+               :blocks =>
+               :slam_dunks =>
               ]
  }
  
