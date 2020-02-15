be9ba16997e9c182e0b60685392e5fede8f79101
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 04:05:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,27 @@
 require 'pry'
 
 def game_hash
+ {
+   :away =>{:team_name => "Charlotte Hornets",
+            :colors => ["Turquoise", "Purple"],
+            :players =>[
+              {:player_name => "Jeff Adrien",
+               :number =>
+               :shoe =>
+               :points =>
+               :rebounds =>
+               :assists =>
+               :steals =>
+               :blocks =>
+               :slam_dunks =>
+              },
+               
+              ]
+ }
+ 
+ 
+ 
+ 
  
 
 def num_points_scored(players)
