473998b0a10df91cb4ea02dd02d6e3d3ddcac167
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 03:56:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -117,10 +117,17 @@ def game_hash
   }
 end
 
-def num_points_score(players_name)
-  game_hash.each do |place,team|
+def num_points_scored(players_name)
+  game_hash.each do |place, team|
     team.each do |attribute, data|
-      if attribute
+      if attribute == :players 
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:points]
+          end
+        end
+      end
+    end
   end 
 end
   
