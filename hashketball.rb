7886b79fac84b6f7ac4b0c44458115b1f2175a56
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 04:14:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,7 +45,7 @@ def game_hash
               :blocks => 1,
               :slam_dunks => 0
                },
-               
+               {:player_name => "Brendan Heywood"
               ]
  }
  
