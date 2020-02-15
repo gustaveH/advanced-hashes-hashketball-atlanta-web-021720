0f5fc6b30be372f6d2ef640a6c8ef91180c820f9
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 05:08:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -158,6 +158,22 @@ def team_names
   end
 end
 
+def player_numbers(team_name)
+  nums = []
+  game_hash.each do |place, team|
+    if team[:team_name]== team_name
+      team.each do |attributes, data|
+        if attributes == :players
+          data.each do |player|
+            nums << player[number]
+          end
+        end
+      end
+    end
+  end
+  nums
+end 
+
 def player_stats(players_name)
   new_hash = {}
   game.hash.each do |place, team|
